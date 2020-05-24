5949e61bd85fa6078f2d054da75490aa6b7b3497
WEGFan <goofans@qq.com>
2020-05-24 20:38:13 +0800

    fix: 修复名字搜索不能使用中括号

---
+++ src/main/java/cn/wegfan/relicsmanagement/config/TomcatConfig.java
@@ -0,0 +1,21 @@
+package cn.wegfan.relicsmanagement.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(connector -> {
+            // 允许特殊字符
+            connector.setProperty("relaxedQueryChars", "|{}[]");
+        });
+        return factory;
+    }
+
+}
