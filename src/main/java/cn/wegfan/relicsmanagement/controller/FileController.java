28fbda066851c2d0dc7b8edbd5f20c680915aec0
WEGFan <goofans@qq.com>
2020-05-28 21:17:12 +0800

    update: 优化文物图片下载的逻辑

--- src/main/java/cn/wegfan/relicsmanagement/controller/FileController.java
+++ src/main/java/cn/wegfan/relicsmanagement/controller/FileController.java
@@ -1,6 +1,5 @@
 package cn.wegfan.relicsmanagement.controller;
 
-import cn.hutool.core.img.ImgUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.wegfan.relicsmanagement.util.BusinessErrorEnum;
 import cn.wegfan.relicsmanagement.util.BusinessException;
@@ -10,12 +9,20 @@ import ma.glasnost.orika.MapperFacade;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.file.Paths;
 
 @Slf4j
@@ -29,19 +36,19 @@ public class FileController {
     /**
      * 获取文物图片
      */
-    @RequestMapping(value = "relics/images/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping(value = "relics/images/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseBody
     @RequiresPermissions(value = {PermissionCodeEnum.ADD_RELIC, PermissionCodeEnum.VIEW_RELIC_INFO}, logical = Logical.OR)
     public byte[] getRelicImage(@PathVariable String fileName) {
-        String dir = Paths.get("data", "images")
+        File file = Paths.get("data", "images")
+                .resolve(fileName)
                 .toAbsolutePath()
-                .toString();
-        String filePath = dir + "/" + fileName;
-        log.debug(filePath);
-        if (!FileUtil.exist(filePath)) {
+                .toFile();
+        log.debug(file.toString());
+        if (!FileUtil.exist(file)) {
             throw new BusinessException(BusinessErrorEnum.FileNotFound);
         }
-        return ImgUtil.toBytes(ImgUtil.read(filePath), "jpg");
+        return FileUtil.readBytes(file);
     }
 
 }
