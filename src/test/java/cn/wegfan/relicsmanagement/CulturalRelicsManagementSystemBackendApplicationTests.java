f2b15410bca654a1ce1c7799058a17323ca393a6
WEGFan <goofans@qq.com>
2020-06-02 16:07:17 +0800

    test: 删除所有临时测试代码

--- src/test/java/cn/wegfan/relicsmanagement/CulturalRelicsManagementSystemBackendApplicationTests.java
+++ src/test/java/cn/wegfan/relicsmanagement/CulturalRelicsManagementSystemBackendApplicationTests.java
@@ -1,24 +1,12 @@
 package cn.wegfan.relicsmanagement;
 
-import cn.wegfan.relicsmanagement.entity.User;
-import cn.wegfan.relicsmanagement.mapper.UserDao;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 class CulturalRelicsManagementSystemBackendApplicationTests {
-
-    @Autowired
-    private UserDao userDao;
-
-    @Test
-    void test1() {
-        User user = userDao.selectById(1);
-        log.debug(user.toString());
-    }
 
     @Test
     void contextLoads() {
